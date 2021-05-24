823baa7e4f75daea64c44522b41a9e75224a4af9
Reggie <75982750+gn01763009@users.noreply.github.com>
2021-05-24 22:29:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello reggie')
+    res.end('Hello reggie we are CD Now !!')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
